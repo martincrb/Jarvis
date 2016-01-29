4e858845493b11b8c6ac89f6220b57289f6653e8
martincrb <mcristobal@cs.upc.edu>
2016-01-29 20:47:18 +0100

    create route widgets

---
+++ app/src/main/java/com/example/innojam/sapinnojam/NewRouteActivity.java
@@ -0,0 +1,52 @@
+package com.example.innojam.sapinnojam;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class NewRouteActivity extends ActionBarActivity {
+
+    EditText name;
+    EditText origin;
+    EditText destination;
+    EditText hour;
+    Button createBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_route);
+
+        name = (EditText) findViewById(R.id.route_name_edit);
+        origin = (EditText) findViewById(R.id.origin_edit);
+        destination = (EditText) findViewById(R.id.destination_edit);
+        hour = (EditText) findViewById(R.id.time_edit);
+        createBtn = (Button) findViewById(R.id.button_create);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_route, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
