e36ee30efed08ba870eeddcd476515cfb3219675
martincrb <mcristobal@cs.upc.edu>
2016-01-29 21:29:04 +0100

    create route widgets

--- app/src/main/java/com/example/innojam/sapinnojam/RoutesActivity.java
+++ app/src/main/java/com/example/innojam/sapinnojam/RoutesActivity.java
@@ -1,17 +1,31 @@
 package com.example.innojam.sapinnojam;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class RoutesActivity extends Activity {
-
+    Button b;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
+        context = this;
+        b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
